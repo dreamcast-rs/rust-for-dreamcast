5f60760da61e0130bd1f750242b0555005769e6a
darc <darcagn@protonmail.com>
2024-02-20 17:29:24 -0600

    Remove need for .cargo/config, add CG_GCCFLAGS option, remove floating point precision patch

--- bin/common.sh
+++ bin/common.sh
@@ -30,7 +30,22 @@ fi
 # Add our toolchain to PATH for the duration of this script
 export PATH="$CG_GCCJIT_GCC_PATH/../bin:$PATH"
 
-# Use -m4-single precision as -m4-single-only is incompatible
-export CG_GCCJIT_SH4_PRECISION="$KOS_SH4_PRECISION"
-
+# rustc_codegen_gcc was compiled in release mode
 export CHANNEL="release"
+
+# Warn user if the KOS_SH4_PRECISION setting is set to something other than -m4-single.
+# Even though we are not using the value from the environment, we should warn the user
+# about a potential mismatch in the configuration.
+if [ ! "$KOS_SH4_PRECISION" == "-m4-single" ] && [ ! "$KOS_SH4_PRECISION" == "" ]; then
+    echo "! WARNING: Your KOS_SH4_PRECISION environment variable is not set to -m4-single!"
+fi
+
+# Add desired flags for rustc to pass to GCC here
+CG_GCCFLAGS="-m4-single -ml -ffunction-sections -fdata-sections -matomic-model=soft-imask -ftls-model=local-exec $CG_GCCFLAGS"
+
+# This will format and add the GCC flags to the CG_RUSTFLAGS for you
+# These will in turn be added to invocations of cargo and rustc
+for flag in $CG_GCCFLAGS; do
+    addflags+="-Cllvm-args=$flag "
+done
+export CG_RUSTFLAGS="$addflags $CG_RUSTFLAGS"
