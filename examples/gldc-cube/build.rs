8983248d3a94149a3223b29bbbe26e48fd7792b2
darc <darcagn@protonmail.com>
2024-12-11 13:09:52 -0600

    Place generated textures in OUT_DIR so they are cleaned

--- examples/gldc-cube/build.rs
+++ examples/gldc-cube/build.rs
@@ -1,42 +1,18 @@
-use std::process::Command;
-
 fn main() {
-    let kos_base = std::env::var("KOS_BASE").expect("Missing $KOS_BASE -- KallistiOS environment not sourced!");
-    let vqenc_cmd = kos_base + "/utils/vqenc/vqenc";
-    Command::new(&vqenc_cmd)
-        .arg("-t")
-        .arg("-v")
-        .arg("rsrc/tex_claw.jpg")
-        .output()
-        .expect("vqenc on tex_claw.jpg failed!");
-    Command::new(&vqenc_cmd)
-        .arg("-t")
-        .arg("-v")
-        .arg("rsrc/tex_dc.jpg")
-        .output()
-        .expect("vqenc on tex_dc.jpg failed!");
-    Command::new(&vqenc_cmd)
-        .arg("-t")
-        .arg("-v")
-        .arg("rsrc/tex_dcwiki.jpg")
-        .output()
-        .expect("vqenc on tex_dcwiki.jpg failed!");
-    Command::new(&vqenc_cmd)
-        .arg("-t")
-        .arg("-v")
-        .arg("rsrc/tex_gcc.jpg")
-        .output()
-        .expect("vqenc on tex_gcc.jpg failed!");
-    Command::new(&vqenc_cmd)
-        .arg("-t")
-        .arg("-v")
-        .arg("rsrc/tex_kos.jpg")
-        .output()
-        .expect("vqenc on tex_kos.jpg failed!");
-    Command::new(&vqenc_cmd)
-        .arg("-t")
-        .arg("-v")
-        .arg("rsrc/tex_rust.jpg")
-        .output()
-        .expect("vqenc on tex_rust.jpg failed!");
+    generate_textures(vec!["tex_claw", "tex_dc", "tex_dcwiki", "tex_gcc", "tex_kos", "tex_rust"]);
+}
+
+fn generate_textures(names: Vec<&str>) {
+    let vqenc_cmd = format!("{}/utils/vqenc/vqenc", std::env::var("KOS_BASE").unwrap());
+
+    for texname in names {
+        let jpgfile = format!("rsrc/{}.jpg", texname);
+        let vqfile = format!("rsrc/{}.vq", texname);
+        let outfile = format!("{}/{}.vq", std::env::var("OUT_DIR").unwrap(), texname);
+        std::process::Command::new(&vqenc_cmd)
+            .args(["-t", "-v", &jpgfile])
+            .output()
+            .expect("vqenc on {&vqfile} failed!");
+        let _ = std::fs::rename(&vqfile, &outfile);
+    }
 }
